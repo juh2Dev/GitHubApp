bfdc5b2044759eb82ebde4ca90017005ce8ce0ab
juh2 <juh2.dev@gmail.com>
2022-12-28 13:38:17 +0900

    [ADD] Tabbar 만들기, 각 탭의 화면 (SearchVC, ProfileVC )생성

---
+++ GitHubApp/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  GitHubApp
+//
+//  Created by 임주희 on 2022/12/28.
+//
+
+import Foundation
+import UIKit
+
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
--- GitHubApp/TabBarViewController.swift
+++ GitHubApp/TabBarViewController.swift
@@ -12,9 +12,52 @@ class TabBarViewController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        attribute()
+    
+       
+        // -----  탭바 설정 ----- //
+        self.delegate = self
+        
+        /* 탭바 색깔 설정
+        self.tabBar.backgroundColor = .darkGray
+        self.tabBar.tintColor = .white
+        */
+        
+        let searchView: SearchViewController = SearchViewController()
+        let profileView: ProfileViewController = ProfileViewController()
+        
+        searchView.tabBarItem = UITabBarItem(title: "search", image: UIImage(systemName: "magnifyingglass"), tag: 0)
+        profileView.tabBarItem = UITabBarItem(title: "profile", image: UIImage(systemName: "person"), tag: 1)
+        self.viewControllers = [searchView, profileView]
+        
+        
+        // 로그인 버튼
+        let loginButton = UIBarButtonItem(title: NSLocalizedString("Login", comment: ""),
+                                        style: .plain,
+                                        target: self,
+                                        action: #selector(loginButtonTapped))
+        navigationItem.rightBarButtonItem = loginButton
+        
+    }
+    
+    private func attribute(){
+        
+        
+        self.title = "GitHub"
+        self.view.backgroundColor = .white
+        
+    }
+    
+    
+    @objc func loginButtonTapped(){
+        print(#fileID, #function)
+    }
+    @objc func logoutButtonTapped(){
         
     }
 }
 
+extension TabBarViewController: UITabBarControllerDelegate {
+    
+}
