efab233e32974ad5b425f8fbc2416343936849ca
juh2 <juh2.dev@gmail.com>
2022-12-29 23:05:18 +0900

    [ADD] ReposigoryTableViewModel 추가

---
+++ GitHubApp/SearchView/RepositoryTableView/RepositoryTableViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  RepositoryTableViewModel.swift
+//  GitHubApp
+//
+//  Created by 임주희 on 2022/12/29.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct RepositoryTableViewModel {
+    
+    
+    
+    
+}
